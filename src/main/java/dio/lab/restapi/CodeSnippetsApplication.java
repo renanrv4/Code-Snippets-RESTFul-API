04a564bcab555472aa5fa319b3f126705263e5e5
renanrv4 <renan.santana.arv@gmail.com>
2025-03-09 00:20:55 -0300

    Initial commit

---
+++ src/main/java/dio/lab/restapi/CodeSnippetsApplication.java
@@ -0,0 +1,13 @@
+package dio.lab.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSnippetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSnippetsApplication.class, args);
+	}
+
+}
